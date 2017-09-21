d16e0c708d2d478cded8fed56c475c678afe2ae2
leason00 <22647085@qq.com>
2017-09-21 15:46:04 +0800

    [U]验证Orm 必须没有构造方法或者构造方法不需要参数

--- app/validate.py
+++ app/validate.py
@@ -5,10 +5,16 @@
 @time: 2017/9/20 16:06
 """
 
+
 def validate(func):
     def inner(session, Orm, datas):
-        # 校验datas
+        # 校验datas -- dict
         if type(datas) is not dict:
             raise TypeError('datas must be dict')
+        # 校验Orm 需要无构造函数或无构造参数
+        try:
+            Orm()
+        except Exception:
+            raise Exception('Orm must have no __init__() or __init__() needs no arguments')
         return func(session, Orm, datas)
     return inner
